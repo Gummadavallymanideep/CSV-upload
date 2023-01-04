ce6ca4b1e00b7a99a1970a7041b971f7174e0d2d
Gummadavallymanideep <107338111+Gummadavallymanideep@users.noreply.github.com>
2023-01-04 19:01:45 +0530

    csv file

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/upload-files');
+mongoose.connect('mongodb+srv://deep:Mani24@projectcluster.tx9gp0v.mongodb.net/?retryWrites=true&w=majority');
 
 const db = mongoose.connection;
 
